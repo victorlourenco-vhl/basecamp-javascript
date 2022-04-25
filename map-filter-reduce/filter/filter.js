044bd78580abd7decfe2fad65c805aa1bdfe9853
Victor Lourenco <victorhlo12@hotmail.com>
2022-04-25 11:17:28 -0300

    filter

---
+++ map-filter-reduce/filter/filter.js
@@ -0,0 +1,11 @@
+function filtrarPares(arr){
+    return arr.filter(callback)
+}
+
+function callback(item){
+    return item % 2 == 0;
+}
+
+const meuArray = [1, 23, 55, 67, 30, 2, 4];
+
+console.log(filtrarPares(meuArray));
