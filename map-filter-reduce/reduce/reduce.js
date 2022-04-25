79ead88f631d69ee1b685e51000588e735fb1738
Victor Lourenco <victorhlo12@hotmail.com>
2022-04-25 11:27:09 -0300

    reduce

---
+++ map-filter-reduce/reduce/reduce.js
@@ -0,0 +1,31 @@
+function somaNumeros(arr){
+    return arr.reduce(function(prev, current){
+        return prev + current;
+    });
+}
+const arr = [1,2];
+console.log(somaNumeros(arr));
+
+const lista = [
+    {
+        name: 'sabao em po', 
+        preco: 30
+    },
+    {
+        name: 'cereal', 
+        preco: 12
+    },
+    {
+        name: 'toalha',
+        preco: 30
+    }
+];
+
+const saldoDisponivel = 100;
+
+function calculaSaldo(saldoDisponivel, lista){
+    return lista.reduce(function(prev, current){
+        return prev - current.preco;
+    }, saldoDisponivel);
+}
+console.log(calculaSaldo(saldoDisponivel, lista));
